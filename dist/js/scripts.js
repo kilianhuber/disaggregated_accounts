86ba1235540ab13e786087116c1ec2ff5e6884a3
Jason Jia <jason.jia@chicagobooth.edu>
2022-10-20 19:37:26 -0500

    initial big commit

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
 * Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
 * Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/disaggregated_accounts/blob/master/LICENSE)
 */
 //
 // Scripts
